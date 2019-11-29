bc1957b963e8203f33c73ae55a76b82f03bc852a
eknaepen <48356198+eknaepen@users.noreply.github.com>
2019-11-29 11:10:56 +0100

    Update candy.h

--- candy.h
+++ candy.h
@@ -1,11 +1,36 @@
 #ifndef CANDY_H
 #define CANDY_H
 
+#include <iostream>
+#include <string>
 
 class Candy
 {
 public:
-    Candy();
+    enum COLOR  // change to graphic's later
+    {
+        RED = 'R',      //1 for randomizer
+        BLUE = 'B',     //2
+        GREEN = 'G',    //3
+        ORANGE = 'O',   //4
+        YELLOW = 'Y'    //5
+    };
+
+    Candy(COLOR color)  : color(color){}
+    ~Candy() {}
+
+    COLOR getColor() const  // get color for combo checker
+    {
+        return color;
+    }
+
+    void print()    // print color for testing
+    {
+        std::cout << color;
+    }
+
+private:
+    const COLOR color;  // can't be changed
 };
 
 #endif // CANDY_H
