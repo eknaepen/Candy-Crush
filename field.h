975c3971ab4524430535b5f7d8021201bcaf47a6
eknaepen <48356198+eknaepen@users.noreply.github.com>
2019-11-29 11:14:09 +0100

    Update field.h

--- field.h
+++ field.h
@@ -1,10 +1,15 @@
 #ifndef FIELD_H
 #define FIELD_H
-
+#include "candy_field.h"
+#include "wall.h"
+#include "random_candy.h"
 
 class Field
 {
 public:
+    unsigned int x;
+    unsigned int y;
+
     Field();
 };
 
