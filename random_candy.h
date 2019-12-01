cf98e821720122db96f1ffa6f581bd7d9b87bdba
eknaepen <48356198+eknaepen@users.noreply.github.com>
2019-12-01 14:54:44 +0100

    Update random_candy.h

--- random_candy.h
+++ random_candy.h
@@ -1,12 +1,18 @@
 #ifndef RANDOM_CANDY_H
 #define RANDOM_CANDY_H
 
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
 #include "candy.h"
 
 class Random_candy
 {
 public:
     Random_candy();
+    ~Random_candy();
+
+    void newCandy(void);
 };
 
 #endif // RANDOM_CANDY_H
