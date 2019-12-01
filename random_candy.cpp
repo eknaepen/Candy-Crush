02c94cd1a83d5de314b433e68a31fc02dade29bd
eknaepen <48356198+eknaepen@users.noreply.github.com>
2019-12-01 14:53:53 +0100

    Update random_candy.cpp

--- random_candy.cpp
+++ random_candy.cpp
@@ -4,3 +4,32 @@ Random_candy::Random_candy()
 {
 
 }
+
+void newCandy()
+{
+    srand((unsigned)time(NULL));
+    int i;
+    i = (rand()%5)+1;
+
+    Candy* candy;
+
+    switch(i)
+    {
+    case 1:
+        candy = new Candy(Candy::RED);
+        break;
+    case 2:
+        candy = new Candy(Candy::BLUE);
+        break;
+    case 3:
+        candy = new Candy(Candy::GREEN);
+        break;
+    case 4:
+        candy = new Candy(Candy::ORANGE);
+        break;
+    case 5:
+        candy = new Candy(Candy::YELLOW);
+        break;
+    }
+    candy->print();
+}
