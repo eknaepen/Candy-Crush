d1bd77a6de44cf2f5f19fe8240bb6fc1dd57122d
eknaepen <48356198+eknaepen@users.noreply.github.com>
2019-12-05 12:21:52 +0100

    Update level.cpp

--- level.cpp
+++ level.cpp
@@ -1,6 +1,24 @@
 #include "level.h"
 
-Level::Level()
+Level::Level() {}
+
+void New_Grid()
+{
+    for(int r=0;r<5;r++)
+    {
+        for(int c=0;c<5;r++)
+        {
+            Field* field = new Field(r,c);
+        }
+    }
+}
+
+void Fill_field(int x, int y)
+{
+
+}
+
+void printGrid()
 {
 
 }
