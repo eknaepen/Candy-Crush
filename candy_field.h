bc9dc1978bd67dd3df04f2e95e4ef9bb562699dc
eknaepen <48356198+eknaepen@users.noreply.github.com>
2019-12-05 12:19:21 +0100

    Update candy_field.h

--- candy_field.h
+++ candy_field.h
@@ -1,6 +1,7 @@
 #ifndef CANDY_FIELD_H
 #define CANDY_FIELD_H
 #include "candy.h"
+#include "random_candy.h"
 
 
 
@@ -8,6 +9,7 @@ class Candy_Field
 {
 public:
     Candy_Field();
+    ~Candy_Field();
 };
 
 #endif // CANDY_FIELD_H
