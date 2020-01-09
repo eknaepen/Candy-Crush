5ea6fcda99f47947f67602e0aa78896d39a2250b
eknaepen <48356198+eknaepen@users.noreply.github.com>
2020-01-09 22:30:13 +0100

    Update random_candy.h

--- random_candy.h
+++ random_candy.h
@@ -9,10 +9,12 @@
 class Random_candy
 {
 public:
-    Random_candy();
-    ~Random_candy();
-
-    void newCandy(void);
+    unsigned char color;
+    Random_candy() {candy = new Candy(Candy::COLOR(rand()%5));      //create a new Candy with random color
+                    color=candy->print();}
+    ~Random_candy() {delete candy;}
+private:
+    Candy * candy;
 };
 
 #endif // RANDOM_CANDY_H
