0a50732078bd311d6d199f898d97ccb4be744ee3
eknaepen <48356198+eknaepen@users.noreply.github.com>
2020-01-09 22:30:39 +0100

    Update wall.h

--- wall.h
+++ wall.h
@@ -1,11 +1,16 @@
 #ifndef WALL_H
 #define WALL_H
+#include <iostream>
+#include "field.h"
 
-
-class Wall
+class Wall : public Field
 {
 public:
-    Wall();
+    Wall(){}
+    unsigned char setsign(){        // create a wall --> candy can't cross
+        sign='|';
+        return sign;
+    }
 };
 
 #endif // WALL_H
