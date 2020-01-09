ac8508f852eeb29f2ca0dd78a171ae919449d3d6
eknaepen <48356198+eknaepen@users.noreply.github.com>
2020-01-09 22:29:06 +0100

    Update level.h

--- level.h
+++ level.h
@@ -1,20 +1,34 @@
 #ifndef LEVEL_H
 #define LEVEL_H
+#include <stdio.h>
+#include <string.h>
+#include "wall.h"
+#include "candy_field.h"
 #include "field.h"
 #include "random_candy.h"
-#include "step_limit.h"
 
 class Level
 {
 public:
+    int score=0;
+    int r;                      // amount of rows
+    int c;                      // amonut of colums
+    unsigned char grid[10][10]; // grid as playing field
 
-    int grid[5][5];
-    Level();
+    Level(int row, int colum);
     ~Level();
 
-    void New_Grid();
-    void Fill_field(int x, int y);
-    void printGrid();
+    void printGrid();   // prints grid
+    void Move();        // make a move on the field
+    int Check_Move(int x, int y, char move[]);  // check if move is legal position wise
+    bool Check_Break_Move(int x, int y);        // check if move is legal combo wise --> no combo no move
+    void Find_Combo();          // check for combo's on the field and replace them by new candy
+
+private:
+    const char* Up="U";
+    const char* Down="D";
+    const char* Left="L";
+    const char* Right="R";
 
 };
 
