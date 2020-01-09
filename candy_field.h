13da77f8c4a8f3fad65c26fdd49dc4e7cb23914f
eknaepen <48356198+eknaepen@users.noreply.github.com>
2020-01-09 22:25:34 +0100

    Update candy_field.h

--- candy_field.h
+++ candy_field.h
@@ -1,15 +1,21 @@
 #ifndef CANDY_FIELD_H
 #define CANDY_FIELD_H
-#include "candy.h"
 #include "random_candy.h"
+#include "field.h"
 
 
-
-class Candy_Field
+class Candy_Field : public Field
 {
 public:
-    Candy_Field();
-    ~Candy_Field();
+    Candy_Field() : rand(new Random_candy){}    // get a new random_candy
+    ~Candy_Field(){delete rand;}
+
+    unsigned char setsign(){    // asign the sign to fill grid
+        sign=rand->color;
+        return sign;
+    }
+private:
+    const Random_candy * rand;
 };
 
 #endif // CANDY_FIELD_H
